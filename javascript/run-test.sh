3f946b28e6e98914c68b8a3970e1c0b942091c88
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-05-03 14:11:12 +0200

    Fixed typo in bash script

--- javascript/run-test.sh
+++ javascript/run-test.sh
@@ -5,10 +5,10 @@
 # If you checkout the public test project as submodule for your assignment as suggested, the compiled game should
 # be located under ../javascript/
 #
-PRAY_AND_HUNTERS_HOME="../javascript"
+PREYS_AND_HUNTERS_HOME=${PREYS_AND_HUNTERS_HOME:-"../javascript"}
 
 if [ $# -lt 1 ]; then echo "Missing test name"; exit 1; fi
 
 # To run the tests you must invoke node passing the pah_home variable from the command line:
 # Be sure you have installed mocha !
-npm --pah_home=${PRAY_AND_HUNTERS_HOME} test
+npm --pah_home=${PREYS_AND_HUNTERS_HOME} test
