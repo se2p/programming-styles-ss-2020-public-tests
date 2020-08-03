df82cdbf4b5c7d69dbdaa81c2ddc0108b76f30c5
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-08-03 14:29:25 +0200

    Marker interfaces to filter tests

---
+++ java/Assignment4.java
@@ -0,0 +1,10 @@
+/**
+ * This is a category marker interface to programmatically filter out tests that
+ * do not belong to Assignment 1
+ * 
+ * @author gambi
+ *
+ */
+public interface Assignment4 {
+
+}
